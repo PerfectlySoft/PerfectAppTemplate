e45beca24c57ad85fc60c0df4835733f82e48736
Jonathan Guthrie <jono@perfect.org>
2017-02-22 10:30:30 -0500

    Adding Utility.makeRequest static function to facilitate making remote requests, and adding CURL and SwiftString packages to support this

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-02-20 23:01:20 +0000
+// Date: 2017-02-22 15:18:29 +0000
 import PackageDescription
 let package = Package(
     name: "Perfect-App-Template",
@@ -9,5 +9,7 @@ let package = Package(
         .Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
         .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2),
+        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1),
     ]
 )
