c57624ccf8e26276745b3317a06032096af26ca7
RockfordWei <rocky@perfect.org>
2018-02-09 16:01:11 -0500

    Upgrading to Swift 4.

--- Package.swift
+++ Package.swift
@@ -5,12 +5,10 @@ let package = Package(
     name: "Perfect-App-Template",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-        .Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
-        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2),
-        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 1),
+      .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+      .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3),
+      .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
+      .Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 3),
+      .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 3),
     ]
 )
